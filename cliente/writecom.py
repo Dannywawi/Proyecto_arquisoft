27436c9dc23ebb5114efc3176c9cded5c9a06218
GabrielRen21 <104105791+GabrielRen21@users.noreply.github.com>
2022-12-14 18:18:30 -0300

    Update writecom.py

--- cliente/writecom.py
+++ cliente/writecom.py
@@ -10,72 +10,39 @@ def WriteCom(type_user, mail):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
 
         alo = ''
-        '''if(type_user == 1 or type_user == 3): # Si el usuario es admin
-            print("(1) Desplegar preguntas del foro\n(2) Responder\n(3) Volver al menu principal")
+        print("Que desea hacer?")
+        print("1. Publicar comentario\n2. Volver al menu principal")
+        
+        try:
             selec = int(input("Seleccione: "))
             if(selec == 1):
-                alo = "desplegar"
-            elif(selec == 2):
-                n = int(input("Ingrese ID de pregunta para responder: "))
-                resp = input("Ingrese respuesta a publicar: ")
-                alo = "responder|"+str(n)+"|"+resp
-            elif(selec == 3):
-                return '''
-        #elif(type_user == 2):
-        if(type_user == 2):
-            #print("(1) Publicar pregunta en el foro\n(2) Desplegar preguntas y respuestas\n(3) Volver al menu principal")
-            #selec = int(input("Seleccione: "))
-            print("(1) Publicar comentario\n(2) Volver al menu principal")
-            selec = int(input("Seleccione: "))
-            if(selec == 1):
-                pal = input("Ingrese comentario: ")
-                alo = "ingresar|"+mail+"|"+pal
-            #elif(selec == 2):
-            #    alo = "desplegar_cli|"+mail
+                pal = input("Ingrese comentario (maximo 200 caracteres): \n")
+                while (len(pal) < 1) or (len(pal) > 200):
+                    pal = input("Ingrese comentario (maximo 200 caracteres): \n")
+                alo = mail+"|||"+pal
             elif(selec == 2):
                 return
-        else:
-            print("Debes registrarte para comentar.")
-            return
-        # Connect the socket to the port where the server is listening
-        server_address = ('localhost', 25000)
-        #print('connecting to {} port {}'.format(*server_address))
-        sock.connect(server_address)
+            else:
+                print("Opcion no disponible.")
+                continue
+            
+            # Connect the socket to the port where the server is listening
+            server_address = ('localhost', 25000)
+            print('connecting to {} port {}'.format(*server_address))
+            sock.connect(server_address)
 
-        try:
-            os.system('clear')
-            # Send data
-            message = alo.encode()
-            #print('sending {!r}'.format(message))
-            sock.sendall(message)
+            if (alo != ''):
+                try:
+                    os.system('clear')
+                    message = alo.encode()
+                    #print('sending {!r}'.format(message))
+                    sock.sendall(message)
 
-            ### Este if no hace nada
-            if(type_user==1 or type_user == 3):
-                if(selec == 1):
-                    data = sock.recv(4096)
-                    for i in pickle.loads(data):
-                        print("=========== ID:",i["ID"],"============")
-                        print("Nombre:",i["nombre"],"| Correo:",i["correo"])
-                        print("Pregunta:",i["descripcion"])
-                elif(selec == 2):
                     data = sock.recv(4096)
                     print(data.decode())
-            
-            #### Se usa este para publicar el comentario
-            #### Se envia el comentario por el socket y luego se lee lo que recibe
-            elif(type_user == 2):
-                #if(selec == 1):
-                data = sock.recv(4096)
-                print(data.decode())
-                '''elif(selec == 2):
-                    data = sock.recv(4096)
-                    for i in pickle.loads(data):
-                        if(i["tipo"]=="pregunta"):
-                            print("=========== ID:",i["ID"],"============")
-                            print("Respondida: "+("si" if i['respondido'] else "no"))
-                            print("Pregunta:",i["descripcion"])
-                        else:
-                            print("Respuesta:",i["descripcion"])'''
-                
-        finally:
-                sock.close()
+                        
+                finally:
+                        sock.close()
+
+        except:
+            print("Opcion invalida.")
